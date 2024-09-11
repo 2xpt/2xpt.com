02e7bd268e84b636250c32475524236409e42f4a
besoeasy <besoeasy@users.noreply.github.com>
2024-09-12 02:50:35 +0530

    Add service section to the website

---
+++ pages/service.vue
@@ -0,0 +1,218 @@
+<template>
+  <div>
+    <section class="py-12 bg-black md:py-24 lg:py-32">
+      <div class="max-w-md mx-auto lg:max-w-none">
+        <div class="flex flex-wrap items-center mb-20 -mx-4">
+          <div class="w-full px-4 mb-8 lg:w-1/2 lg:mb-0">
+            <h3
+              class="max-w-md text-6xl font-bold text-yellow-500 font-heading"
+            >
+              IT CONSULTING
+            </h3>
+          </div>
+          <div class="w-full px-4 lg:w-1/2">
+            <Lottie loc="/ani/circle.json" />
+          </div>
+        </div>
+        <div class="flex flex-wrap -mx-4">
+          <div class="w-full px-4 mb-12 lg:w-1/3 lg:mb-0">
+            <Featdis
+              id="1"
+              title="Workflow Automation"
+              subtitle="Automate repetitive tasks, streamline operations. Custom solutions for data entry, document processing, and more."
+            />
+          </div>
+          <div class="w-full px-4 mb-12 lg:w-1/3 lg:mb-0">
+            <Featdis
+              id="2"
+              title="Data Analysis"
+              subtitle="Expert data analysis, visualization, and reporting. Custom solutions for data mining, predictive analytics, and more."
+            />
+          </div>
+          <div class="w-full px-4 lg:w-1/3">
+            <Featdis
+              id="3"
+              title="Bookkeeping"
+              subtitle="Expert bookkeeping, accounting, and financial analysis. Custom solutions for payroll, invoicing, and more."
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="py-12 bg-black md:py-24 lg:py-32">
+      <div class="max-w-md mx-auto lg:max-w-none">
+        <div class="flex flex-wrap items-center mb-20 -mx-4">
+          <div class="w-full px-4 mb-8 lg:w-1/2 lg:mb-0">
+            <h3
+              class="max-w-md text-6xl font-bold text-yellow-500 font-heading"
+            >
+              COMPUTER FORENSICS
+            </h3>
+          </div>
+          <div class="w-full px-4 lg:w-1/2">
+            <Lottie loc="/ani/circle.json" />
+          </div>
+        </div>
+        <div class="flex flex-wrap -mx-4">
+          <div class="w-full px-4 mb-12 lg:w-1/3 lg:mb-0">
+            <Featdis
+              id="1"
+              title="Lost Crypto Recovery"
+              subtitle="Reclaim lost cryptocurrencies. Expert assistance for forgotten passwords, inaccessible wallets, ensuring streamlined recovery."
+            />
+          </div>
+          <div class="w-full px-4 mb-12 lg:w-1/3 lg:mb-0">
+            <Featdis
+              id="2"
+              title="Hacked Crypto Recovery"
+              subtitle="Restore hacked assets, fortify security. Expert investigation, recovery strategies for compromised accounts, thefts."
+            />
+          </div>
+          <div class="w-full px-4 lg:w-1/3">
+            <Featdis
+              id="3"
+              title="Blockchain Forensics"
+              subtitle="Trace cyberattacks, identify hackers. Advanced analysis, digital footprints tracing within blockchain networks."
+            />
+          </div>
+
+          <div class="w-full px-4 mb-12 lg:w-1/3 lg:mb-0">
+            <Featdis
+              id="4"
+              title="Digital Forensics"
+              subtitle="Investigate digital crimes, data breaches. Expert analysis, evidence collection, and reporting for legal proceedings."
+            />
+          </div>
+
+          <div class="w-full px-4 mb-12 lg:w-1/3 lg:mb-0">
+            <Featdis
+              id="5"
+              title="Mobile Forensics"
+              subtitle="Recover lost data, investigate mobile crimes. Expert analysis, evidence collection, and reporting for legal proceedings."
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="py-12 bg-black md:py-24 lg:py-32">
+      <div class="max-w-md mx-auto lg:max-w-none">
+        <div class="flex flex-wrap items-center mb-20 -mx-4">
+          <div class="w-full px-4 mb-8 lg:w-1/2 lg:mb-0">
+            <h3
+              class="max-w-md text-6xl font-bold text-yellow-500 font-heading"
+            >
+              PROJECT DEVELOPMENT
+            </h3>
+          </div>
+          <div class="w-full px-4 lg:w-1/2">
+            <Lottie loc="/ani/circle.json" />
+          </div>
+        </div>
+        <div class="flex flex-wrap -mx-4">
+          <div class="w-full px-4 mb-12 lg:w-1/3 lg:mb-0">
+            <Featdis
+              id="01"
+              title="Smart Contract Development"
+              subtitle="Custom smart contract development, deployment, and auditing. Tailored solutions for decentralized applications, tokenization, and more."
+            />
+          </div>
+          <div class="w-full px-4 mb-12 lg:w-1/3 lg:mb-0">
+            <Featdis
+              id="02"
+              title="Token Development"
+              subtitle="Token creation, issuance, and management. Customized token development, including security tokens, utility tokens, and more."
+            />
+          </div>
+          <div class="w-full px-4 lg:w-1/3">
+            <Featdis
+              id="03"
+              title="Decentralized Applications"
+              subtitle="Custom DApp development, deployment, and maintenance. Expert solutions for decentralized finance, NFTs, and more."
+            />
+          </div>
+          <div class="w-full px-4 lg:w-1/3">
+            <Featdis
+              id="04"
+              title="Blockchain Consulting"
+              subtitle="Expert guidance, strategic planning, and advisory services. Comprehensive blockchain consulting for startups, enterprises, and more."
+            />
+          </div>
+
+          <div class="w-full px-4 lg:w-1/3">
+            <Featdis
+              id="05"
+              title="Blockchain Integration"
+              subtitle="Seamless blockchain integration, migration, and maintenance. Expert solutions for integrating blockchain into existing systems, applications."
+            />
+          </div>
+
+          <div class="w-full px-4 lg:w-1/3">
+            <Featdis
+              id="06"
+              title="Blockchain Security"
+              subtitle="Comprehensive security solutions for blockchain networks. Expert security audits, penetration testing, and more."
+            />
+          </div>
+
+          <div class="w-full px-4 lg:w-1/3">
+            <Featdis
+              id="07"
+              title="Bot Development"
+              subtitle="Custom bot development, deployment, and maintenance. Expert solutions for trading bots, NFT bots, and more."
+            />
+          </div>
+
+          <div class="w-full px-4 lg:w-1/3">
+            <Featdis
+              id="08"
+              title="NFT Development"
+              subtitle="Custom NFT development, deployment, and maintenance. Expert solutions for NFT marketplaces, NFT games, and more."
+            />
+          </div>
+
+          <div class="w-full px-4 lg:w-1/3">
+            <Featdis
+              id="09"
+              title="Website Development"
+              subtitle="Custom website development, deployment, and maintenance. Expert solutions for static websites, dynamic websites, and more."
+            />
+          </div>
+
+          <div class="w-full px-4 lg:w-1/3">
+            <Featdis
+              id="10"
+              title="Mobile App Development"
+              subtitle="Custom mobile app development, deployment, and maintenance. Expert solutions for Android, iOS, and more."
+            />
+          </div>
+
+          <div class="w-full px-4 lg:w-1/3">
+            <Featdis
+              id="11"
+              title="Desktop App Development"
+              subtitle="Custom desktop app development, deployment, and maintenance. Expert solutions for Windows, macOS, and more."
+            />
+          </div>
+
+          <div class="w-full px-4 lg:w-1/3">
+            <Featdis
+              id="12"
+              title="API Development"
+              subtitle="Custom API development, deployment, and maintenance. Expert solutions for RESTful APIs, GraphQL APIs, and more."
+            />
+          </div>
+
+          <div class="w-full px-4 lg:w-1/3">
+            <Featdis
+              id="13"
+              title="E-commerce Development"
+              subtitle="Custom e-commerce development, deployment, and maintenance. Expert solutions for Shopify, WooCommerce, and more."
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
