1d694ef12157aede9cbee03c36900d845db14114
besoeasy <besoeasy@users.noreply.github.com>
2024-09-13 13:26:35 +0530

    Refactor Xbut component styles to use rounded corners and update hover animation

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,6 +1,174 @@
 <template>
-    <main>
-       <ContentDoc />
-    </main>
-  </template>
-  
+  <main>
+    <ContentDoc />
+
+    <section class="relative py-12 md:py-24">
+      <div class="">
+        <nuxtLink
+          :to="`/blog`"
+          class="inline-flex items-center mb-12 font-bold text-white hover:text-teal-600"
+        >
+          <svg
+            width="20"
+            height="20"
+            viewbox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M15.4167 10H5M5 10L10 5M5 10L10 15"
+              stroke="currentColor"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+          </svg>
+          <span class="ml-2">Back to Blog</span>
+        </nuxtLink>
+        <img
+          class="block object-cover w-full mb-12 rounded-xl"
+          src="https://shuffle.dev/aurora-assets/content/folders-dark.png"
+          alt=""
+        />
+        <div class="max-w-4xl mx-auto">
+          <div class="flex items-center mb-4">
+            <span class="text-sm text-gray-400">20 Jul 2023</span>
+          </div>
+          <h2
+            class="mb-12 text-4xl font-bold tracking-tight text-white font-heading sm:text-5xl"
+          >
+            How We Built AI-Collections: Wrapping Up
+          </h2>
+          <div class="mb-12">
+            <h4 class="mb-6 text-3xl font-bold text-white">Introduction</h4>
+            <p class="mb-6 text-lg text-gray-300">
+              As we come to the end of our journey in building AI-Collections,
+              we reflect on the incredible experience and the technological
+              advancements that have shaped this groundbreaking project. From
+              its inception, our goal was to create a sophisticated system that
+              leverages the power of artificial intelligence to curate and
+              organize digital collections seamlessly. The journey involved a
+              dedicated team of engineers, data scientists, and designers who
+              collaborated tirelessly to bring this vision to life. We'll delve
+              into the challenges we faced, the technologies we utilized, and
+              the exciting potential AI-Collections holds for the future.
+            </p>
+            <span class="block text-lg text-gray-300"
+              >1. Defining the Vision</span
+            >
+            <span class="block text-lg text-gray-300"
+              >2. Team Collaboration</span
+            >
+            <span class="block text-lg text-gray-300"
+              >3. Data Acquisition and Management</span
+            >
+          </div>
+          <div class="mb-12">
+            <h4 class="mb-6 text-3xl font-bold text-white">General content</h4>
+            <p class="mb-6 text-lg text-gray-300">
+              The development process was marked by countless hours of research,
+              prototyping, and iteration. We explored various machine learning
+              algorithms and deep neural network architectures to ensure optimal
+              performance and accuracy in content classification. Through
+              rigorous testing and fine-tuning, we trained our AI model to
+              recognize patterns, categorize assets, and recommend relevant
+              collections with impressive precision.
+            </p>
+            <div class="flex mb-6">
+              <span class="block mr-2 text-lg text-gray-300">1.</span>
+              <div>
+                <p>
+                  <span class="inline-block mr-1 text-lg font-bold text-white"
+                    >Defining the Vision:</span
+                  >
+                  <span class="text-lg text-gray-300"
+                    >We begin by discussing the initial vision behind
+                    AI-Collections and the objectives we aimed to achieve. We
+                    highlight the need for a sophisticated content curation
+                    system that leverages AI capabilities to enhance user
+                    experiences and streamline information discovery.</span
+                  >
+                </p>
+              </div>
+            </div>
+            <div class="flex mb-6">
+              <span class="block mr-2 text-lg text-gray-300">2.</span>
+              <div>
+                <p>
+                  <span class="inline-block mr-1 text-lg font-bold text-white"
+                    >Team Collaboration:</span
+                  >
+                  <span class="text-lg text-gray-300"
+                    >Successful projects require effective teamwork. We
+                    emphasize the importance of collaboration and detail how our
+                    team of experts, including developers, data scientists, and
+                    designers, worked together to bring AI-Collections to life.
+                    We share insights on how we fostered a creative and
+                    innovative environment throughout the development
+                    process.</span
+                  >
+                </p>
+              </div>
+            </div>
+            <div class="flex">
+              <span class="block mr-2 text-lg text-gray-300">3.</span>
+              <div>
+                <p>
+                  <span class="inline-block mr-1 text-lg font-bold text-white"
+                    >Data Acquisition and Management:</span
+                  >
+                  <span class="text-lg text-gray-300"
+                    >Content demands a reliable and extensive dataset. We
+                    provide an overview of the strategies we employed to gather
+                    and manage the data required for AI-Collections. This
+                    includes sourcing relevant content, data cleaning, and
+                    ensuring data quality and diversity.</span
+                  >
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="pb-12 mb-12 border-b border-gray-700">
+            <div class="mb-12">
+              <h4 class="mb-6 text-3xl font-bold text-white">
+                Additional reading
+              </h4>
+              <p class="mb-6 text-lg text-gray-300">
+                Design played a crucial role in the development of
+                AI-Collections. We wanted to create an intuitive user interface
+                that empowers users to effortlessly manage their digital assets.
+                Our design team conducted extensive user research and feedback
+                sessions to understand the pain points and needs of content
+                creators. The result is a sleek, user-friendly interface that
+                allows for seamless navigation, quick bookmarking, and efficient
+                organization of assets.
+              </p>
+              <p class="text-lg text-gray-300">
+                Throughout the process, we prioritized data privacy and
+                security. We implemented robust encryption protocols and strict
+                access controls to ensure that sensitive user information
+                remains protected at all times. Building trust with our users
+                has been of utmost importance, and we are committed to
+                maintaining a secure environment for their valuable assets.
+              </p>
+            </div>
+            <div class="mb-12">
+              <h4 class="mb-6 text-3xl font-bold text-white">Conclusion</h4>
+              <p class="text-lg text-gray-300">
+                As we wrap up the development phase, we're excited about the
+                potential AI-Collections holds for content creators, marketers,
+                and businesses alike. Our AI-powered solution brings a new level
+                of efficiency and productivity to digital asset management,
+                freeing up valuable time and resources for creativity and
+                innovation. We're grateful for the opportunity to contribute to
+                the ever-evolving landscape of AI technology, and we look
+                forward to seeing how AI-Collections transforms the way assets
+                are curated and organized in the digital world.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
