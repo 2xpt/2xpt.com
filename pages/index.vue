9f02f429ea67205ab4bc6b1c0d1fd39212e6c0f9
Aman Singh Katal <besoeasy@users.noreply.github.com>
2025-01-20 03:06:22 +0530

    0

--- pages/index.vue
+++ pages/index.vue
@@ -5,19 +5,10 @@
         <Lottie loc="ani/home.json" />
       </div>
       <div class="w-full lg:w-2/5">
-        <div
-          class="flex flex-col items-stretch justify-center h-full sm:items-start"
-        >
-          <div
-            class="my-5 text-2xl font-bold tracking-widest text-yellow-300 lg:text-4xl hover:text-white"
-          >
-            2 X P T
-          </div>
+        <div class="flex flex-col items-stretch justify-center h-full sm:items-start">
+          <div class="my-5 text-2xl font-bold tracking-widest text-yellow-300 lg:text-4xl hover:text-white">2 X P T</div>
 
-          <div class="my-3 font-mono text-base tracking-wide lg:text-xl">
-            We deliver innovative, secure, and customized technology solutions
-            that drive organizational success.
-          </div>
+          <div class="my-3 font-mono text-base tracking-wide lg:text-xl">We deliver innovative, secure, and customized technology solutions that drive organizational success.</div>
 
           <div class="my-3 uppercase">
             <div class="relative inline-block mr-3">
@@ -30,9 +21,11 @@
           </div>
 
           <div class="my-3 font-mono text-base tracking-wide uppercase">
-            <a href="mailto:2xpt@proton.me" class="text-yellow-300">
-              2xpt@proton.me
-            </a>
+            <a href="mailto:2xpt@proton.me" class="text-yellow-300"> 2xpt@proton.me </a>
+
+            <a href="https://t.me/+M_aVipwr_NUzMGE1" class="text-yellow-300"> Telegram Group </a>
+
+            <a href="https://x.com/2xptcom" class="text-yellow-300"> X </a>
           </div>
         </div>
       </div>
